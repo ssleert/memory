e1d09fb8d47b62f507aed3c492c87d1486a7d72b
ssleert <semenilichevsky@gmail.com>
2023-01-04 02:13:53 +0300

    init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/ssleert/memory
+
+go 1.19
+
+require github.com/ssleert/sfolib v0.0.0-20230103064514-0606fb9e8dbf
---
+++ memory.go
@@ -0,0 +1,70 @@
+package memory
+
+import (
+	"strconv"
+	"strings"
+
+	"github.com/ssleert/sfolib"
+)
+
+type Ram struct {
+	Total     int
+	Free      int
+	Available int
+}
+
+const (
+	MemInfo = "/proc/meminfo"
+)
+
+// parse value from line in /proc/meminfo
+func parseMemValue(v string) int {
+	in, _ := strconv.Atoi(strings.Fields(v)[1])
+	return in
+}
+
+// get Ram struct from /proc/meminfo
+func GetRam() (Ram, error) {
+	lines, err := sfolib.ReadLines(MemInfo, 3)
+	if err != nil {
+		return Ram{}, err
+	}
+
+	ram := Ram{
+		Total:     parseMemValue(lines[0]) / 1024,
+		Free:      parseMemValue(lines[1]) / 1024,
+		Available: parseMemValue(lines[2]) / 1024,
+	}
+
+	return ram, nil
+}
+
+// get total ram amount
+func GetTotalRam() (int, error) {
+	line, err := sfolib.ReadFirstLine(MemInfo)
+	if err != nil {
+		return 0, err
+	}
+
+	return parseMemValue(line) / 1024, nil
+}
+
+// get free ram amount
+func GetFreeRam() (int, error) {
+	line, err := sfolib.ReadLine(MemInfo, 2)
+	if err != nil {
+		return 0, err
+	}
+
+	return parseMemValue(line) / 1024, nil
+}
+
+// get available ram amount
+func GetAvalibleRam() (int, error) {
+	line, err := sfolib.ReadLine(MemInfo, 3)
+	if err != nil {
+		return 0, err
+	}
+
+	return parseMemValue(line) / 1024, nil
+}
