a1129bd881822487c521bb5f5ad8d04297f358ff
ssleert <ssleert@sfome.my.domain>
2023-02-16 17:36:26 +0300

    Update: added swap support

--- memory.go
+++ memory.go
@@ -11,6 +11,8 @@ type Ram struct {
 	Total     int
 	Free      int
 	Available int
+	SwapTotal int
+	SwapFree  int
 }
 
 const (
@@ -25,7 +27,7 @@ func parseMemValue(v string) int {
 
 // get Ram struct from /proc/meminfo
 func GetRam() (Ram, error) {
-	lines, err := sfolib.ReadLines(MemInfo, 3)
+	lines, err := sfolib.ReadLines(MemInfo, 16)
 	if err != nil {
 		return Ram{}, err
 	}
@@ -34,6 +36,8 @@ func GetRam() (Ram, error) {
 		Total:     parseMemValue(lines[0]),
 		Free:      parseMemValue(lines[1]),
 		Available: parseMemValue(lines[2]),
+		SwapTotal: parseMemValue(lines[14]),
+		SwapFree:  parseMemValue(lines[15]),
 	}
 
 	return ram, nil
@@ -62,6 +66,24 @@ func GetFreeRam() (int, error) {
 // get available ram amount
 func GetAvalibleRam() (int, error) {
 	line, err := sfolib.ReadLine(MemInfo, 3)
+	if err != nil {
+		return 0, err
+	}
+
+	return parseMemValue(line), nil
+}
+
+func GetSwapTotal() (int, error) {
+	line, err := sfolib.ReadLine(MemInfo, 15)
+	if err != nil {
+		return 0, err
+	}
+
+	return parseMemValue(line), nil
+}
+
+func GetSwapFree() (int, error) {
+	line, err := sfolib.ReadLine(MemInfo, 16)
 	if err != nil {
 		return 0, err
 	}
